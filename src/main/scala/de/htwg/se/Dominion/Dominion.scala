d146ae2907440a85fa8a3517c3350fcdac1df5b4
Karsten <huberkarsten@gmail.com>
2020-05-29 10:23:11 +0200

    players addable through input

--- src/main/scala/de/htwg/se/Dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/Dominion/Dominion.scala
@@ -2,9 +2,16 @@ package de.htwg.se.Dominion
 
 import de.htwg.se.Dominion.model.{Board, Card, CardSet, Player, Tui}
 
+import scala.collection.mutable.ListBuffer
+
 object Dominion {
   def main(args: Array[String]): Unit = {
     val tui = Tui()
+
+    val players = tui.getPlayerList()
+
+
+
     tui.printTui()
   }
 }
--- src/main/scala/de/htwg/se/Dominion/model/Tui.scala
+++ src/main/scala/de/htwg/se/Dominion/model/Tui.scala
@@ -1,14 +1,40 @@
 package de.htwg.se.Dominion.model
 
+import scala.collection.mutable.ListBuffer
+
 case class Tui() {
   def printTui(): Unit = {
 
-    println("Dominion\n")
-    val student1 = Player("Jakob Strakhof")
-    val student2 = Player("Karsten Huber")
-    println("Hello, " + student1.name + " and " + student2)
+//    println("Dominion\n")
+//    val student1 = Player("Jakob Strakhof")
+//    val student2 = Player("Karsten Huber")
+//    println("Hello, " + student1.name + " and " + student2)
 
     val board = Board().toString()
     print(board)
   }
+
+  def readPlayerCount(): Int = {
+    print("Input number of Players: ")
+    scala.io.StdIn.readInt()
+  }
+
+  def getPlayerNames(playerCount: Int = readPlayerCount()) : List[String] = {
+    var playerList = new ListBuffer[String]()
+    for (x <- 1 to playerCount) {
+      print("Input player " + x  + " name: ")
+      playerList += scala.io.StdIn.readLine()
+    }
+    playerList.toList
+  }
+
+  def getPlayerList(playerNames: List[String] = getPlayerNames()): List[Player] = {
+    var playerListTemp = new ListBuffer[Player]
+    for (x <- playerNames) {
+      playerListTemp += Player(x)
+    }
+    val playerList = playerListTemp.toList
+    playerList
+  }
+
 }
