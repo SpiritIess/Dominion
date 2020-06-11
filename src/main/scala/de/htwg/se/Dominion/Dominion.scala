c5ea05371e78e5e0521dd514930d6409f1a87869
Spiritless <jakobstrakhof@gmail.com>
2020-06-11 16:41:36 +0200

    updated dominion object and tui

--- src/main/scala/de/htwg/se/Dominion/Dominion.scala
+++ src/main/scala/de/htwg/se/Dominion/Dominion.scala
@@ -5,28 +5,21 @@ import de.htwg.se.Dominion.model.{Board, Card, CardSet, Hand, Pile, Player, Play
 
 object Dominion {
   val tui = new Tui
+
   def main(args: Array[String]): Unit = {
-    println("Dominion\n")
-    val student1 = Player("Jakob Strakhof")
-    val student2 = Player("Karsten Huber")
-    println("Hello, " + student1.name + " and " + student2)
+    var input: String = ""
+    var test = 0
 
-    val board = Board().toString
-    print(board)
-    /*
-    println(student1.startingPile.toString)
-    println(student1.startingHand.toString)
-    println(student1.playerDrawPile.toString)
-
-    println(student2.startingPile.toString)
-    println(student2.startingHand.toString)
-    println(student1.playerDrawPile.toString)
-     */
-    var input : String = ""
     do {
-      input = readLine()
-      tui.processInputLine(input,student1)
+      tui.printTui()
+      input = scala.io.StdIn.readLine()
+      test = tui.processInputLine(input)
+      //      println("ProcessInput Return: " + test)
     } while (input != "q")
+
+
+
+
 
   }
 }
